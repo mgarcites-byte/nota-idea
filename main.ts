4d8c87cc2af0a5dafe5908c51daede4d26113c78
mgarcites-byte <mgarcites@gmail.com>
2026-06-21 19:43:06 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+/**
+ * Nota de las ideas
+ * 
+ * 1.Al encender la casa poner en la pantalla LCD ¡Hola Mario!
+ * 
+ * y un símbolo de cargando ha continuación poner casa de Mario
+ * 
+ * poner el LED en modo sube
+ */
